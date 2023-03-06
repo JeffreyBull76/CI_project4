a114468465098472e89ecdbb856b079e91e4ba88
JeffreyBull76 <jeffreybull@sky.com>
2023-03-06 18:24:38 +0000

    add base models and admin functions

--- digigallery/admin.py
+++ digigallery/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('prompt', 'negprompt', 'method')
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'prompt', 'negprompt', 'method']
+    list_filter = ('status', 'created_on')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    actions = ['approve_comments']
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'body')
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
