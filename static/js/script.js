42810786772bbc82635fff0e4be54ad8c9f65f93
JeffreyBull76 <jeffreybull@sky.com>
2023-03-17 01:41:06 +0000

    add intersection loader

---
+++ static/js/script.js
@@ -0,0 +1,19 @@
+/** https://www.youtube.com/watch?v=2IbRtjez6ag **/
+document.addEventListener("DOMContentLoaded", function(){
+
+    const cards = document.querySelectorAll(".card")
+    
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry =>{
+            entry.target.classList.toggle("show", entry.isIntersecting)
+            })
+    },
+    {
+        threshold: 0,
+    }
+    )
+    
+    cards.forEach(card => {
+        observer.observe(card)
+    })
+})
